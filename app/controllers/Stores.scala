337497fd6dcba39b5f5d6245736745b39afe8de7
Mark Wunsch <mark@markwunsch.com>
2013-04-16 23:23:14 -0400

    WS call to API for active sales
    Demonstrates Future composition and Async behavior. Given the API is
    available, this will result in a 401 Unauthorized.

--- app/controllers/Stores.scala
+++ app/controllers/Stores.scala
@@ -3,8 +3,26 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import scala.concurrent.Future
+import play.api.libs._
+import play.api.libs.concurrent.Execution.Implicits._
+
 object Stores extends Controller {
 
-  def activeSalesByKey(store: String) = TODO
+
+  def activeSalesByKey(store: String) = Action {
+    val eventualResponse: Future[ws.Response] = {
+      ws.WS.url(API_ROOT+"sales/"+store+"/active.json").get()
+    }
+
+    Async {
+      eventualResponse map { response =>
+        Logger.info("API Response: %s %s".format(response.status, response.statusText))
+        Status(response.status) { response.body }
+      }
+    }
+  }
+
+  private val API_ROOT = "https://api.gilt.com/v1/"
 
 }
